f0b5e913e8242eb9b0f0b5ec0f395a8c5e993500
Chris Casey <chrisjohncasey@gmail.com>
2025-06-12 20:12:26 +1200

    gaps

--- R/GraphicsPages.R
+++ R/GraphicsPages.R
@@ -8,9 +8,11 @@
 #' @param groupVar Name of the variable for grouping (e.g., "RegionDesc1").
 #' @param funnelVars Character vector of 3 variables to filter progressively.
 #' @param funnelVals List of values (same length as funnelVars) used for filtering.
-#' @param colours Named list of colours, including `highlight`.
+#' @param colours Named list of colours, including `highlight`, `text1`, `text2`, `hitext`, `borders`.
 #' @param years Numeric vector of years to include.
+#' @param groupGap Numeric, vertical gap between groups in units of row height (e.g., 0.5 = half a row height).
 #' @param DEBUG Logical flag for debug mode.
+#'
 #' @export
 DrawDrowningFunnel <- function(
     pi,
@@ -21,17 +23,27 @@ DrawDrowningFunnel <- function(
     funnelVals,
     colours = list(highlight = "#BF0000"),
     years = 2000:2024,
+    groupGap = 0,
     DEBUG = FALSE
 ) {
-  PlotBasePage(path = path, pagesize = "A3", orientation = "landscape", DEBUG = DEBUG)
+
+  textcols=c(colours$text1,colours$text1, colours$text2,colours$hitext)
+  PlotBasePage(path = path, pagesize = "A3", orientation = "portrait", DEBUG = DEBUG)
   PlotPageHeader(pi, colT = colours$highlight)
 
   groups = unique(df[[groupVar]])
   nGroups = length(groups)
   nYears = length(years)
 
+  headerLines = 4
+  nVars = length(funnelVars) + 1
+  totalRows = nGroups * (nVars + groupGap) + headerLines
+  ygap = 2 / totalRows
+
   xgap = 1 / nYears
-  ygap = 2 / (nGroups * 4)
+
+  leftadj=0.025
+
 
   for (g in seq_along(groups)) {
     region = groups[g]
@@ -39,8 +51,8 @@ DrawDrowningFunnel <- function(
     for (i in seq_along(years)) {
       yr = years[i]
       datYr = datRegion[datRegion$Year == yr, ]
-      y0 = 2 - (g - 1) * 4 * ygap
-      x0 = (i - 1) * xgap
+      y0 = 2 - (g - 1) * 4 * ygap - (g - 1) * groupGap * ygap - headerLines *ygap
+      x0 = (i - 1) * xgap + leftadj
       counts = rep(NA, 4)
       counts[1] = nrow(datYr)
       for (j in 1:3) {
@@ -49,29 +61,34 @@ DrawDrowningFunnel <- function(
       }
       for (j in 1:4) {
         yy = y0 - (j - 1) * ygap
+
+        rect(x0, yy - ygap, x0 + xgap, yy,
+             border = colours$borders , lwd = 1)
+
         if(counts[j])
         {
-          fontVal = if (j == 1) 2 else 1  # 2 = bold, 1 = normal
+          fontval = if (j == 1) 2 else 1  # 2 = bold, 1 = normal
+          textcol = textcols[j]
 
           rect(x0, yy - ygap, x0 + xgap, yy,
                col = ifelse(j == 4, colours$highlight, "white"),
                border = colours$borders , lwd = 1)
 
           text(x0 + xgap/2, yy - ygap/2, labels = counts[j],
-               col = ifelse(j == 4, "white", "black"), cex = 0.7, font = fontVal)
+               col = textcol, cex = 1, font = fontval)
         }
       }
     }
     # Draw region label to the left
-    text(-0.01, 2 - (g - 1) * 4 * ygap - 2 * ygap,
-         labels = region, adj = 1, xpd = TRUE, cex = 0.7)
+    text(-0.01+leftadj,  2 - (g - 1) * 4 * ygap - (g - 1) * groupGap * ygap - headerLines *ygap,
+         labels = region, adj = c(1,1), xpd = TRUE, cex = 0.7,font=2)
   }
 
   # Draw year labels at top
   for (i in seq_along(years)) {
     x0 = (i - 1) * xgap
-    rect(x0, 2 + 0.002, x0 + xgap, 2 + 0.04, col = "black", border = NA)
-    text(x0 + xgap/2, 2 + 0.02, labels = years[i], adj = c(0.5, 0.5), col = "white", cex = 0.8, xpd = TRUE)
+    rect(x0+leftadj, 2 + 0.002- headerLines *ygap, x0 + xgap+leftadj, 2 + 0.04 - headerLines *ygap, col = "black", border = NA)
+    text(x0 + xgap/2+leftadj, 2 + 0.02- headerLines *ygap, labels = years[i], adj = c(0.5, 0.5), col = "white", cex = 0.8, xpd = TRUE)
   }
 
   PlotPageFooter()
